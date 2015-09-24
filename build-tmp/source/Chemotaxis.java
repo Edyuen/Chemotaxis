895d0924f0d35a13f50200d2305810fd3859c62d
unknown <eddiesfe@hotmail.com>
2015-09-24 10:57:07 -0700

    changed stuff

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,9 +1,25 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] colony;
 Bacteria edward;
 PImage photo;
 PImage photo1;
 PImage photo2;
- void setup()   
+ public void setup()   
  {     
  photo=loadImage("singed.png");
  photo1=loadImage("kaptain.png");
@@ -24,7 +40,7 @@ PImage photo2;
  	}
 
  }   
- void draw()   
+ public void draw()   
  {    
 
  image(photo2,0,0);
@@ -39,7 +55,7 @@ PImage photo2;
  }  
 
 
- void mousePressed()
+ public void mousePressed()
  	{
  		fill(255);
  		ellipse(mouseX,mouseY, 10,10);
@@ -52,11 +68,11 @@ PImage photo2;
  	{
  		myX=x;
  		myY=y;
- 		kolor = color(int(random(256+100)),int(random(256+100)),int(random(256+100)));
+ 		kolor = color(PApplet.parseInt(random(256+100)),PApplet.parseInt(random(256+100)),PApplet.parseInt(random(256+100)));
 
  	}
 
- 	void move()
+ 	public void move()
  	{
  		//myX = myX + (int)(Math.random()*5-2.5);
  		//myY = myY + (int)(Math.random()*5-2.5);
@@ -83,7 +99,7 @@ PImage photo2;
  	}
 
  	
- 	void show()
+ 	public void show()
  	{
  		//fill(int(random(256+100)),int(random(256+100)),int(random(256+100)));
 
@@ -106,3 +122,12 @@ PImage photo2;
 
  	}
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
